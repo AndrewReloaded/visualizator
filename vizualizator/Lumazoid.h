f0e9094e1f05f1d681e03f2e2db7033108851e4b
AndrewReloaded <andrew_my@inbox.ru>
2018-12-16 22:47:54 +0300

    scetch added

---
+++ vizualizator/Lumazoid.h
@@ -0,0 +1,27 @@
+/*
+  Realtime music visualizer board
+  Firmware Michael Krumpus,
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License
+*/
+
+#ifndef LUMAZOID_H
+#define LUMAZOID_H
+
+#if (ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+ #include <pins_arduino.h>
+#endif
+
+typedef struct {
+  uint8_t baseColor;
+  uint8_t age;
+  uint8_t magnitude;
+  uint8_t rnd;
+} peak_t;
+
+
+#endif
+
